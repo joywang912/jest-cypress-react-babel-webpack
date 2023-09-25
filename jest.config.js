421324c411b1b8968a6518ff398db5957f16f853
Joy Wang <joywang912@hotmail.com>
2023-09-25 15:04:26 +1300

    commit ~10

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
+const path = require('path')
+
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  snapshotSerializers: ['@emotion/jest/serializer'],
 }
